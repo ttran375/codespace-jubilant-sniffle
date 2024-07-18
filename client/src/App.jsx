88c49ce4259c97ce586e58981f0a19b586438a89
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-18 18:27:58 +0000

    Add mern example

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/styles';
-import MainRouter from '../MainRouter';
-import theme from '../theme';
-//import { hot } from 'react-hot-loader'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Login from "./pages/users/Login";
+import Register from "./pages/users/Register";
+import Dashboard from "./pages/users/Dashboard";
+import Home from "./pages/posts/Home";
+import Create from "./pages/posts/Create";
+import Update from "./pages/posts/Update";
+import AuthRoutes from "./Routes/AuthRoutes";
+import GuestRoutes from "./Routes/GuestRoutes";
+
 const App = () => {
- return (
-<Router>
-<ThemeProvider theme={theme}>
-<MainRouter />
-</ThemeProvider>
-</Router>
- );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route element={<AuthRoutes />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="create" element={<Create />} />
+            <Route path="update" element={<Update />} />
+          </Route>
+
+          <Route element={<GuestRoutes />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
